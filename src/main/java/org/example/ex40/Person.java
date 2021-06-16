86977ef1f74556763316683c3faa29abcd1f18fb
Anisha Desktop <anisharanjan55@gmail.com>
2021-06-15 23:29:09 -0400

    Initial commit

---
+++ src/main/java/org/example/ex40/Person.java
@@ -0,0 +1,15 @@
+package org.example.ex40;
+
+public class Person {
+    String firstName;
+    String lastName;
+    String position;
+    String separation_date;
+
+    /*public Person(String firstName, String lastName, String position, String separation_date) {
+        this.firstName = "";
+        this.lastName = "";
+        this.position = "";
+        this.separation_date = "";
+    }*/
+}
